7bd6118b400174bb2b624c39c08ac3cfb5f86923
houssem eddine massoudi <houssem-eddine.lmasoudi@enis.tn>
2022-01-05 15:54:15 +0100

    hi

---
+++ src/main/java/com/example/exman2020spring/Exman2020springApplication.java
@@ -0,0 +1,13 @@
+package com.example.exman2020spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exman2020springApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exman2020springApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exman2020spring/Exman2020springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exman2020spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exman2020springApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
